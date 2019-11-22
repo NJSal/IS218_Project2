e04732484deb35ffb3a0f96ada95232932d38150
David <dfs23@njit.edu>
2019-11-22 17:27:45 -0500

    fix: added email and password back to login page

--- displayquestion.php
+++ displayquestion.php
@@ -1,15 +1,27 @@
 <?php
 require('pdo.php');
-
+/*
 $firstname = filter_input(INPUT_GET, 'fname');
 //$lastname = filter_input(INPUT_GET, 'lname');
 //$birthday = filter_input(INPUT_GET, 'birthday');
 $email = filter_input(INPUT_GET, 'email');
+*/
 /*$password = filter_input(INPUT_GET, 'password');*/
 
-$idselect = filter_input(INPUT_GET, 'ownerid');
 
-echo "User Name: $firstname <br>";
+/*
+$idselect = filter_input(INPUT_GET, 'ownerid');
+*/
+
+$query = 'SELECT fname, lname from accountsIS218 WHERE email = :email';
+$statement = $db->prepare($query);
+$statement->bindValue(':email', $email);
+$statement->bindValue(':fname', $firstname);
+$statement->bindValue(':lname', $lastname);
+$statement->execute();
+
+echo "First Name: $firstname <br>";
+echo "Last Name: $lastname <br>";
 echo "Email: $email<br>";
 
 $query = 'SELECT title, body from questions WHERE email = :email and password = :password';
@@ -19,6 +31,8 @@ $statement->bindValue(':email', $email);
 $statement->bindValue(':password', $password);
 $statement->execute();
 $values= $statement->fetchAll();
+
+
 
 $statement->closeCursor();
 
